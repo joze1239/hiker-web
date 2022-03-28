4fecff6de0298df8f93c3a058ae7248a165f07bb
Joze Fartek <joze.fartek@gmail.com>
2022-03-29 00:56:07 +0200

    Update Map search styles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -16,4 +16,32 @@ export const theme: StyleConfig = extendTheme({
       },
     },
   },
+  styles: {
+    global: {
+      '.mapboxgl-ctrl-geocoder': {
+        height: '30px',
+        fontSize: '14px',
+      },
+      '.mapboxgl-ctrl-geocoder.mapboxgl-ctrl-geocoder--collapsed': {
+        width: '30px',
+        minWidth: '30px',
+      },
+      '.mapboxgl-ctrl-geocoder--icon': {
+        top: '5px',
+        left: '5px',
+        width: '20px',
+        height: '20px',
+      },
+      '.mapboxgl-ctrl-geocoder--icon-close': {
+        marginTop: '-1px',
+        marginRight: 0,
+        width: '16px',
+        height: '16px',
+      },
+      '.mapboxgl-ctrl-geocoder--input': {
+        height: '30px',
+        padding: '6px 35px',
+      },
+    },
+  },
 });
